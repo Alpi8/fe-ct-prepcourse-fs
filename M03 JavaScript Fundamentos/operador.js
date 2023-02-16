21812e8875d91b4c32c6d652c60f83a2497e44ed
jusog <jusog281299@gmail.com>
2023-02-16 15:33:47 -0500

    Revert "Solucionado"
    This reverts commit 52efc5a8b25b43ce478fa8e21449e319235ce263.

--- M03 JavaScript Fundamentos/operador.js
+++
@@ -1,9 +0,0 @@
-letra = "a";
-if  ( letra === ("a" || "e" || "i" || "o" || "u" || "A" || "E" || "I" || "O" || "U") )  {
-    console.log ("Es vocal")
-    return; 
-    } else if  ( letra.length > 1 ) { 
-    console.log ("Dato incorrecto") }    
-            else  (letra != "a" || "e" || "i" || "o" || "u" || "A" || "E" || "I" || "O" || "U")
-             console.log ("Dato incorrecto");
-          
